1ae03632bfd201835f1cefb3f580cd399ba734cc
sibaram <sibaram@LAPTOP-9QU59GPI>
2018-08-29 16:00:54 +0530

    i did this class

--- Testproject/src/ecommerce.java
+++ Testproject/src/ecommerce.java
@@ -1,4 +1,16 @@
 
 public class ecommerce {
+	
+	public void rent() {
+		System.out.println("this is good critical");
+		
+	}
+	public static void main(String args[]) {
+		
+		ecommerce s=new ecommerce();
+		s.rent();
+		
+		
+	}
 
 }
