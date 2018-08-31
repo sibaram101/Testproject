e601a14d645f3dd229e552d3518f5a154ff9b7cf
sibaram <sibaram@LAPTOP-9QU59GPI>
2018-08-31 06:37:17 +0530

    i did gust method

--- Testproject/src/ecommerce.java
+++ Testproject/src/ecommerce.java
@@ -5,6 +5,9 @@ public class ecommerce {
 		System.out.println("this is good critical");
 		
 	}
+	void gust() {
+		System.out.println("people willnot stay more then 2 days");
+	}
 	public static void main(String args[]) {
 		
 		ecommerce s=new ecommerce();
